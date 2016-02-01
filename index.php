e4fb444e73358526e2ad2e63c4c1f0165f0b3002
David Baines <dave@lucolo.com>
2016-02-01 13:39:06 -0500

    Finished processor

--- index.php
+++ index.php
@@ -6,13 +6,9 @@
 	<body>
 		<h1>Upload a file and make some messages!</h1>
 		<h4>Upload a CSV .txt file (with at least 30 rows) with email, name, and ZIP code fields to make messages based on the below template:</h4>
-		<form method='post' action='/processor.php'>
-			<textarea name='template' disabled style='resize:none;width:500px;height:200px;'>Hi @@name,
-
-I'm reaching out from my email, @@email, to see if you're around @@zip today in order to meet.
-
-Let me know and thanks!
-Alex</textarea>
+		<form action='/processor.php' method='post' enctype='multipart/form-data'>
+			<textarea name='template' style='resize:none;width:200px;height:100px;'>Hi @@name. I'm reaching out from my email, @@email, to see if you're around @@zip today in order to meet. Let me know and thanks! Alex</textarea>
+			<br>
 			<br>
 			<input type='file' name='file'><br><br>
 			<input type='submit' value='Generate Messages!'>
--- processor.php
+++ processor.php
@@ -1,5 +1,17 @@
 <?php
 
-	
+	// get the contents of the text file and put it into an array
+	$file = file_get_contents($_FILES['file']['tmp_name']);
+	$rows = array_map('str_getcsv', file($_FILES['file']['tmp_name']));
 
+	$template = $_POST['template'];
+
+	foreach($rows as $row)
+	{
+		$message = str_replace('@@name', $row[0], $template);
+		$message = str_replace('@@email', $row[1], $message);
+		$message = str_replace('@@zip', $row[2], $message);
+
+		echo $message.'<br><br>';
+	}
 ?>
